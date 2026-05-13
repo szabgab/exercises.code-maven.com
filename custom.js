f9d71717affb491f5192540e395c1378233d5200
Gabor Szabo <gabor@szabgab.com>
2026-05-13 14:03:14 +0300

    display csv and txt files as well

--- custom.js
+++ custom.js
@@ -1,4 +1,4 @@
 const prefixes = ["src/examples/"];
-const extensions = [".py", ".toml", ".cfg"];
+const extensions = [".py", ".toml", ".cfg", ".csv", ".txt"];
 const base_url = "https://github.com/szabgab/python.code-maven.com/tree/main/books/python-testing/"
 
