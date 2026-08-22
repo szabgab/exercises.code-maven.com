a3e636226fd8866b8422319c66d6111dd1e5327c
Gabor Szabo <gabor@szabgab.com>
2026-08-22 13:12:54 +0300

    fix

--- examples/python/recursive_count_target_expressions.py
+++ examples/python/recursive_count_target_expressions.py
@@ -1,14 +1,18 @@
 def count_target_expressions(numbers, target):
     # TODO verify that all the numbers and the target are non-negative integers.
-    if numbers == []:
-        return 0
-    if len(numbers) == 1:
-        if numbers[0] == target or -lst[0] == target:
+    if not numbers:
+        if target == 0:
             return 1
         else:
             return 0
 
-    return count_target_expressions(numbers[1:], target-lst[0]) + count_target_expressions(lst[1:], target+lst[0])
+    plus = count_target_expressions(numbers[1:], target-numbers[0])
+    minus = count_target_expressions(numbers[1:], target+numbers[0])
+
+    return plus + minus
+
+assert count_target_expressions([], 0) == 1
+assert count_target_expressions([], 2) == 0
 
 assert count_target_expressions([0], 1) == 0
 assert count_target_expressions([1], 1) == 1
